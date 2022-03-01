d4b604da55fabc7aa94b79420735d559fab55251
Nadia Collado <collado.nadia@gmail.com>
2022-03-01 14:01:37 -0500

    Add dropdown menu

--- react-playground/src/GeneApp.test.tsx
+++ react-playground/src/GeneApp.test.tsx
@@ -1,12 +1,7 @@
-import { getByText, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import GeneApp from "./GeneApp";
 
 test("renders a message", () => {
   render(<GeneApp />);
-  expect(screen.getByText("Hello Nadia!")).toBeInTheDocument();
-});
-
-test("renders the header component", () => {
-  render(<GeneApp />);
-  expect(screen.getByText("This is the header!")).toBeInTheDocument();
+  expect(screen.getByText("Hello")).toBeInTheDocument();
 });
